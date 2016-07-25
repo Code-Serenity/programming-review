fb10522500f60ffdc09fb9531c7c525936764c77
arrrnv <anv_1991@hotmail.com>
2016-07-24 22:20:26 -0500

    add class code

--- Hello World/Hello World/Source.cpp
+++ Hello World/Hello World/Source.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-	cout << "Hello World, visual studio commit!!!" << endl;
+class Student {
 
+public:
+	//my broken class
+	Student(int a, string b) { a = age; b = name; };
+	int getage() { return age; };
+	string getname() { return name; };
+
+private:
+	int age;
+	string name;
+
+};
+
+int main()
+{
+	int i = 1;
+	Student S1(5, "Joe");
+	Student S2(6, "Jan");
+	Student S3(7, "Jac");
+
+
+	cout << "Student " << i << " name is " << S1.getname() << " and age is " << S1.getage() << "." << endl;
 	system("pause");
 	return 0;
 }
