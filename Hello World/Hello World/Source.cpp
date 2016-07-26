42a6c08fe7f49d5b9ce12dbbfaf5f4f87127a33a
arrrnv <anv_1991@hotmail.com>
2016-07-25 22:00:55 -0500

    change the whole code

--- Hello World/Hello World/Source.cpp
+++ Hello World/Hello World/Source.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
 #include <string>
+
+// Link-List Example
+
 using namespace std;
 
-class Student {
-
-public:
-	//my broken class
-	Student(int a, string b) { a = age; b = name; };
-	int getage() { return age; };
-	string getname() { return name; };
-
-private:
-	int age;
-	string name;
-
+class Node {
+	int data;
+	Node* next;
 };
 
 int main()
 {
-	int i = 1;
-	Student S1(5, "Joe");
-	Student S2(6, "Jan");
-	Student S3(7, "Jac");
+	/**/
+	Node *n, *t, *h;
 
+	n = new Node;
+	n->data = 1;
 
-	cout << "Student " << i << " name is " << S1.getname() << " and age is " << S1.getage() << "." << endl;
 	system("pause");
 	return 0;
 }
